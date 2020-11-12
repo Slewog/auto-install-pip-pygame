712fdb6c1b6fc2d10e456f4e31591e615e60959c
Slewog <31140215+Slewog@users.noreply.github.com>
2020-11-12 02:06:58 +0100

    code optimization

--- auto install pip-pygame/required.py
+++ auto install pip-pygame/required.py
@@ -8,67 +8,59 @@ class CheckRequiredModules:
     def __init__(self):
         self.pygame_is_installed = True
         self.tkinter_is_installed = True
-        self.win32_error = "[EXCEPTION] Windows system operating is executing."
-        self.modules_update_linux_1 = "python3 -m pip list -o > outdated"
-        self.modules_update_linux_2 = "for x in $(sed -n '3,$p' outdated  awk '{print $1}'); do python3 -m pip install -U $x; done"
-        self.tkinter_install_linux = "sudo apt-get install python3-tk"  # "sudo apt-get remove python3-tk"
-        self.tcl_install_linux = "sudo apt-get install tcl"  # " pip install Tcl"
-        self.pip_update_linux = "python3 -m pip install -U pip"
-        self.pip_install_linux = "sudo apt-get install python3-pip"
-        self.pip_update_win32 = "python -m pip install -U pip"
-        self.pip_install_win32 = "python get-pip.py"
-        self.wheel_install_linux = "python3 -m pip install wheel"
-        self.wheel_install_win32 = "python -m pip install wheel"
-        self.setuptools_install_linux = "python3 -m pip install setuptools"
-        self.setuptools_install_win32 = "python -m pip install setuptools"
-        self.pygame_install_linux = "python3 -m pip install pygame"  # pip3 uninstall pygame
-        self.pygame_install_win32 = "pip install pygame"
+        self.python_call_linux = "python3 "
+        self.python_call_win32 = "python "
 
     def modules_update(self):
+        pip_update = "-m pip install -U pip"
+        modules_update_list = "-m pip list -o > outdated"
+        modules_update_linux = "for x in $(sed -n '3,$p' outdated  awk '{print $1}'); do python3 -m pip install -U $x; done"
         try:
-            print("[UPDATE] Trying to update pip module")
+            print("[UPDATE] Trying to update your modules")
             if sys.platform == 'linux':
-                subprocess.run(self.pip_update_linux, shell=True)
-                print("[UPDATE] Trying to update other modules")
-                subprocess.run(self.modules_update_linux_1, shell=True)
-                subprocess.run(self.modules_update_linux_2, shell=True)
+                for command in [self.python_call_linux + pip_update, self.python_call_linux + modules_update_list, modules_update_linux]:
+                    subprocess.run(command, shell=True)
                 try:
                     os.remove(r"outdated")
                 except OSError as e:
                     print('[ERROR] the file "outdated" does not exist.')
             elif sys.platform == 'win32':
-                subprocess.run(self.pip_update_win32, shell=True)
-                print("[UPDATE] Trying to update other modules")
-                subprocess.run("python -m pip list -o > outdated", shell=True)
-                f = open("outdated", "r")
-                module_list = f.readlines()
-                f.close()
-                module_list.pop(0), module_list.pop(0)
-                for module in module_list:
-                    module_list = module.replace("  ", ",")
-                    module_list = list(module_list.split(","))
-                    subprocess.run("python -m pip install " + module_list[0] + " -U", shell=True)
+                for command in [pip_update, modules_update_list]:
+                    subprocess.run(self.python_call_win32 + command, shell=True)
+                with open("outdated") as outdated_file:
+                    module_list = outdated_file.readlines()
+                if len(module_list) >= 3:
+                    module_list.pop(0), module_list.pop(0)
+                    for module in module_list:
+                        module_list = module.replace(" ", ",")
+                        module_list = list(module_list.split(","))
+                        subprocess.run("python -m pip install " + module_list[0] + " -U", shell=True)
                 try:
                     os.remove(r"outdated")
-                except OSError as e:
-                    print('[ERROR] the file "outdated" does not exist.')
+                except OSError:
+                    print('[ERROR] The file "outdated" does not exist.')
             print("[UPDATE] Your python modules has been updated")
         except:
             print("[UPDATE ERROR] Your python modules has not been updated")
 
     def pip_install(self):
+        pip_install_linux = "sudo apt-get install python3-pip"
+        pip_install_win32 = "python get-pip.py"
+        wheel_install = "-m pip install wheel"
+        setuptools_install = "-m pip install setuptools"
         print("[INSTALLATION] Trying to install pip")
         if sys.platform == 'linux':
-            subprocess.run(self.pip_install_linux, shell=True)
-            subprocess.run(self.setuptools_install_linux, shell=True)
-            subprocess.run(self.wheel_install_linux, shell=True)
+            for command in [pip_install_linux, self.python_call_linux + setuptools_install, self.python_call_linux + wheel_install]:
+                subprocess.run(command, shell=True)
         elif sys.platform == 'win32':
-            subprocess.run(self.pip_install_win32, shell=True)
-            subprocess.run(self.setuptools_install_win32, shell=True)
-            subprocess.run(self.wheel_install_win32, shell=True)
+            for command in [pip_install_win32, self.python_call_win32 + setuptools_install, self.python_call_win32 + wheel_install]:
+                subprocess.run(command, shell=True)
         print("[INSTALLATION] Pip and module has been installed")
 
     def tkinter_check(self):
+        win32_error = "[EXCEPTION] Windows system operating is executing."
+        tkinter_install_linux = "sudo apt-get install python3-tk"  # "sudo apt-get remove python3-tk"
+        tcl_install_linux = "sudo apt-get install tcl"  # " pip install Tcl"
         try:
             print("[IMPORT] Trying to import tkinter")
             import tkinter as tk
@@ -78,17 +70,17 @@ class CheckRequiredModules:
             try:
                 print("[INSTALLATION] Trying to install tkinter")
                 if sys.platform == "linux":
-                    subprocess.run(self.tcl_install_linux, shell=True)
-                    subprocess.run(self.tkinter_install_linux, shell=True)
+                    for command in [tcl_install_linux, tkinter_install_linux]:
+                        subprocess.run(command, shell=True)
+                    self.tkinter_is_installed = False
                 elif sys.platform == "win32":
                     self.tkinter_is_installed = False
-                    raise NameError(self.win32_error)
-                self.tkinter_is_installed = False
+                    raise NameError(win32_error)
                 import tkinter as tk
                 print("[INSTALLATION] Tkinter has been installed")
             except:
                 if sys.platform == "win32":
-                    print(self.win32_error)
+                    print(win32_error)
                     print('[EXCEPTION] Please repair your version of python with module Tcl/Tk and pip!!!')
                 elif sys.platform == "linux":
                     try:
@@ -96,8 +88,8 @@ class CheckRequiredModules:
                         self.pip_install()
                         try:
                             print("[INSTALLATION] Trying to install tkinter")
-                            subprocess.run(self.tcl_install_linux, shell=True)
-                            subprocess.run(self.tkinter_install_linux, shell=True)
+                            for command in [tcl_install_linux, tkinter_install_linux]:
+                                subprocess.run(command, shell=True)
                             import tkinter as tk
                             print("[INSTALLATION] Tkinter has been installed")
                             self.tkinter_is_installed = False
@@ -107,36 +99,36 @@ class CheckRequiredModules:
                         print("[INSTALLATION ERROR] Pip could not be installed")
 
     def pygame_check(self):
+        pygame_install = "-m pip install pygame"  # pip3 uninstall pygame
         try:
             print("[IMPORT] Trying to import pygame")
             import pygame
             print("[IMPORT] Pygame is installed")
         except:
             print("[EXCEPTION] Pygame not installed")
-
             try:
                 print("[INSTALLATION] Trying to install pygame via pip")
                 if sys.platform == "linux":
-                    subprocess.run(self.pygame_install_linux, shell=True)
+                    subprocess.run(self.python_call_linux + pygame_install, shell=True)
                 elif sys.platform == "win32":
-                    subprocess.run(self.pygame_install_win32, shell=True)
+                    subprocess.run(self.python_call_win32 + pygame_install, shell=True)
                 import pygame
                 print("[INSTALLATION] Pygame has been installed")
                 self.pygame_is_installed = False
             except:
                 try:
-                    print("[EXCEPTION] pip and module not installed on system")
+                    print("[EXCEPTION] Pip not installed on system")
                     self.pip_install()
                     try:
                         print("[INSTALLATION] Trying to install pygame")
                         if sys.platform == "linux":
-                            subprocess.run(self.pygame_install_linux, shell=True)
+                            subprocess.run(self.python_call_linux + pygame_install, shell=True)
                         elif sys.platform == "win32":
-                            subprocess.run(self.pygame_install_win32, shell=True)
+                            subprocess.run(self.python_call_win32 + pygame_install, shell=True)
                         import pygame
-                        print("[INSTALLATION] pygame has been installed")
+                        print("[INSTALLATION] Pygame has been installed")
                         self.pygame_is_installed = False
                     except:
-                        print("[INSTALLATION ERROR] pygame could not be installed")
+                        print("[INSTALLATION ERROR] Pygame could not be installed")
                 except:
                     print("[INSTALLATION ERROR] Pip could not be installed")
